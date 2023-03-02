3b2276f8538cba8d6ed40d77986ea0754df4c70e
maciejeicam <maciej_michalik@onet.pl>
2023-03-02 22:33:05 +0100

    Initial Commit

---
+++ WeatherApp_SwiftUI/WeatherApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_SwiftUIApp.swift
+//  WeatherApp_SwiftUI
+//
+//  Created by Maciej Michalik on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
